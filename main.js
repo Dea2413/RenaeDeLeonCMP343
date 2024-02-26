587b7c9714d996bd0bb8ce3d07747c8a2e5e0de2
Renae DeLeon <renaedeleon24@gmail.com>
2024-02-26 13:43:38 -0500

    Reorder JS

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ console.log('JS is now offically linked up');
 
 /*------ Constants -----*/
 
+/*----- App's state (varibles) ----*/
+let board; 
+let turn = 'X'; 
+
 /*----- Cached element refences ----*/
 const squares = Array.from(document.querySelectorAll('#board div')); 
 const messages = document.querySelector('h2'); 
 
-/*----- App's state (varibles) ----*/
-let board; 
-let turn = 'X'; 
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn); 
 
 /*----- functions -----*/ 
-//Turns 
 function handleTurn(event){ 
     let idx = squares.findIndex(function(square){ 
         return square === event.target; 
@@ -27,16 +27,6 @@ turn = turn === 'X' ? 'O' : 'X';
 console.log(board); 
 }; 
 
-//Render function 
-function render(){ 
-    board.forEach(function(mark, index){ 
-        console.log(mark, index);
-        squares[index].textContent = mark; 
-    });
-    
-    messages.textContent = `It's ${turn}'s turn!`; 
-}; 
-
 //Board function 
 function init(){ 
     board = [
@@ -44,12 +34,20 @@ function init(){
         '', '', '', 
         '', '', ''
     ]; 
+
+    //Render function 
+    function render(){ 
+        board.forEach(function(mark, index){ 
+           console.log(mark, index);
+           squares[index].textContent = mark; 
+        });
+        
+        messages.textContent = `It's ${turn}'s turn`; 
+    }; 
     render(); 
 }; 
 
 init(); 
-
-
 
 
 
