d4de9e6261db44a56a64f15b2bf434db091b3bbc
Renae DeLeon <renaedeleon24@gmail.com>
2024-02-26 12:44:03 -0500

    Progress save - addded message changes

--- main.js
+++ main.js
@@ -4,10 +4,38 @@ console.log('JS is now offically linked up');
 
 /*----- Cached element refences ----*/
 const squares = Array.from(document.querySelectorAll('#board div')); 
+const messages = document.querySelector('h2'); 
 
 /*----- App's state (varibles) ----*/
 let board; 
 let turn = 'X'; 
+
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn); 
+
+/*----- functions -----*/ 
+//Turns 
+function handleTurn(event){ 
+    let idx = squares.findIndex(function(square){ 
+        return square === event.target; 
+    }); 
+
+board[idx] = turn; 
+
+turn = turn === 'X' ? 'O' : 'X'; 
+
+console.log(board); 
+}; 
+
+//Render function 
+function render(){ 
+    board.forEach(function(mark, index){ 
+        console.log(mark, index);
+        squares[index].textContent = mark; 
+    });
+    
+    messages.textContent = `It's ${turn}'s turn!`; 
+}; 
 
 //Board function 
 function init(){ 
@@ -16,32 +44,11 @@ function init(){
         '', '', '', 
         '', '', ''
     ]; 
-
-    //Render function 
-    function render(){ 
-        board.forEach(function(mark, index){ 
-           console.log(mark, index);
-           squares[index].textContent = mark; 
-        }); 
-    }; 
     render(); 
 }; 
 
 init(); 
 
-/*----- event listeners -----*/
-document.getElementById('board').addEventListener('click', handleTurn); 
-
-/*----- functions -----*/ 
-function handleTurn(event){ 
-    let idx = squares.findIndex(function(square){ 
-        return square === event.target; 
-    }); 
-
-board[idx] = turn; 
-
-console.log(board); 
-}; 
 
 
 
