662d1431a58e8515f01ffd611ffaad5d2c98b8ae
Renae DeLeon <renaedeleon24@gmail.com>
2024-02-26 12:31:44 -0500

    Added JS to add event when clicked and add the information to the board

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
 console.log('JS is now offically linked up'); 
+
+/*------ Constants -----*/
+
+/*----- Cached element refences ----*/
+const squares = Array.from(document.querySelectorAll('#board div')); 
+
+/*----- App's state (varibles) ----*/
+let board; 
+let turn = 'X'; 
+
+//Board function 
+function init(){ 
+    board = [
+        '', '', '', 
+        '', '', '', 
+        '', '', ''
+    ]; 
+
+    //Render function 
+    function render(){ 
+        board.forEach(function(mark, index){ 
+           console.log(mark, index);
+           squares[index].textContent = mark; 
+        }); 
+    }; 
+    render(); 
+}; 
+
+init(); 
+
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn); 
+
+/*----- functions -----*/ 
+function handleTurn(event){ 
+    let idx = squares.findIndex(function(square){ 
+        return square === event.target; 
+    }); 
+
+board[idx] = turn; 
+
+console.log(board); 
+}; 
+
+
+
+
